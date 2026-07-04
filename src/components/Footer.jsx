cd3b873cd3bff62429a70224b9dd91cc1f44ffc8
Hugo Jariod Tomas <hugojariod77@gmail.com>
2026-07-04 13:29:30 +0200

    añadido fontawesome; creado header y footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import logo from '../assets/logo_ataju.svg'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { Link } from 'react-router-dom'
+
+library.add(fas, far, fab)
 
 const Footer = () => {
+
+    const currentYear = new Date().getFullYear();
+
     return (
         <>
-            <div>Footer</div>
+            <footer className="text-gray-600 fixed bottom-0 w-full">
+                <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+                    <Link to={"/"} className="flex items-center md:justify-start justify-center text-gray-900 hover:cursor-pointer">
+                        <img src={logo} alt="" className='w-20 h-20' />
+                    </Link>
+                    <p className="text-lg text-gray-500 font-title sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+                        © {currentYear} ATAJÚ Teatro
+                    </p>
+                    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start text-xl">
+                        <a href='https://www.instagram.com/ataju_teatro' className="text-ataju-green hover:cursor-pointer">
+                            <FontAwesomeIcon icon="fa-brands fa-instagram"></FontAwesomeIcon>
+                        </a>
+                        <a href='https://x.com/ataju_teatro' className="ml-3 text-ataju-green hover:cursor-pointer">
+                            <FontAwesomeIcon icon="fa-brands fa-x-twitter"></FontAwesomeIcon>
+                        </a>
+                        <a href='https://youtube.com/@atajuteatro' className="ml-3 text-ataju-green hover:cursor-pointer">
+                            <FontAwesomeIcon icon="fa-brands fa-youtube"></FontAwesomeIcon>
+                        </a>
+                    </span>
+                </div>
+            </footer>
         </>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import Navbar from './Navbar'
 const Header = () => {
     return (
         <>
-            <div className='w-full min-h-20 p-10 flex justify-between border-b-2 border-royal-gold'>
+            {/* <div className='w-full min-h-20 p-10 flex justify-between border-b-2 border-royal-gold'>
                 <Link to={'/'} className='flex flex-col items-center gap-4'>
                     <img src={logo} className='h-30 w-30' />
 
@@ -14,7 +14,18 @@ const Header = () => {
                 <div className='my-auto'>
                     <Navbar></Navbar>
                 </div>
-            </div>
+            </div> */}
+            <header className="text-ataju-green">
+                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-center">
+                    <nav className='flex flex-wrap items-center justify-center'>
+                        <a className="hover:text-gray-900 md:mx-7 mx-2 md:text-xl font-title hover:cursor-pointer pr-5 border-r">Sobre Nosotros</a>
+                        <a className="flex order-first sm:order-0 w-full md:w-fit font-medium justify-center items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0 hover:cursor-pointer">
+                            <img src={logo} className='w-25 h-25 md:w-30 md:h-30' />
+                        </a>
+                        <a className="hover:text-gray-900 md:mx-7 md:text-xl mx-2 font-title hover:cursor-pointer md:pl-5 md:border-l">Nuestras Obras</a>
+                    </nav>
+                </div>
+            </header>
         </>
     )
 }
