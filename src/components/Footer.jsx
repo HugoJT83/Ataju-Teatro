403fea37b99323b1c2c0fb8165ff72df29af049a
Hugo Jariod Tomas <hugojariod77@gmail.com>
2026-07-05 13:29:09 +0200

    añadido header responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 library.add(fas, far, fab)
 
@@ -19,13 +19,13 @@ const Footer = () => {
         <>
             <footer className="text-gray-600 fixed bottom-0 w-full">
                 <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-                    <Link to={"/"} className="flex items-center md:justify-start justify-center text-gray-900 hover:cursor-pointer">
-                        <img src={logo} alt="" className='w-20 h-20' />
-                    </Link>
-                    <p className="text-lg text-gray-500 font-title sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+                    <NavLink to={"/"} className="flex items-center md:justify-start justify-center text-gray-900 hover:cursor-pointer">
+                        <img src={logo} alt="" className='w-25 h-25' />
+                    </NavLink>
+                    <p className="text-xl text-gray-500 font-title sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
                         © {currentYear} ATAJÚ Teatro
                     </p>
-                    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start text-xl">
+                    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start text-4xl">
                         <a href='https://www.instagram.com/ataju_teatro' className="text-ataju-green hover:cursor-pointer">
                             <FontAwesomeIcon icon="fa-brands fa-instagram"></FontAwesomeIcon>
                         </a>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,59 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
 import logo from '../assets/logo_ataju_vectorizado.svg'
-import Navbar from './Navbar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <>
-            {/* <div className='w-full min-h-20 p-10 flex justify-between border-b-2 border-royal-gold'>
-                <Link to={'/'} className='flex flex-col items-center gap-4'>
-                    <img src={logo} className='h-30 w-30' />
-
-                </Link>
-
-                <div className='my-auto'>
-                    <Navbar></Navbar>
-                </div>
-            </div> */}
             <header className="text-ataju-green">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-center">
-                    <nav className='flex flex-wrap items-center justify-center'>
-                        <a className="hover:text-gray-900 md:mx-7 mx-2 md:text-xl font-title hover:cursor-pointer pr-5 border-r">Sobre Nosotros</a>
-                        <a className="flex order-first sm:order-0 w-full md:w-fit font-medium justify-center items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0 hover:cursor-pointer">
+
+                    {/* Diseño para Desktop */}
+                    <nav className='hidden md:flex flex-wrap items-center justify-center'>
+                        <NavLink to={"/nosotros"} className="hover:text-gray-900 md:text-xl px-3 font-title hover:cursor-pointer border-r">
+                            Sobre Nosotros
+                        </NavLink>
+
+                        <NavLink to={"/talleres"} className="hover:text-gray-900 md:text-xl px-3 font-title hover:cursor-pointer border-r">
+                            Talleres
+                        </NavLink>
+                        <NavLink to={"/"} className="flex order-first sm:order-0 w-full px-7 md:w-fit font-medium justify-center items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0 hover:cursor-pointer">
                             <img src={logo} className='w-25 h-25 md:w-30 md:h-30' />
-                        </a>
-                        <a className="hover:text-gray-900 md:mx-7 md:text-xl mx-2 font-title hover:cursor-pointer md:pl-5 md:border-l">Nuestras Obras</a>
+                        </NavLink>
+                        <NavLink to={"/obras"} className="hover:text-gray-900 md:text-xl px-3 font-title hover:cursor-pointer border-l">Nuestras Obras</NavLink>
+                        <NavLink to={"/contacto"} className="hover:text-gray-900 md:text-xl px-3 font-title hover:cursor-pointer border-l">Contacto</NavLink>
                     </nav>
+
+                    {/* Diseño para movil */}
+                    <div className='md:hidden w-full flex justify-between'>
+                        <NavLink to={"/"} className="hover:cursor-pointer">
+                            <img src={logo} className='w-20 h-20 md:w-30 md:h-30' />
+                        </NavLink>
+                        <button
+                            onClick={() => setIsOpen(!isOpen)}
+                            className='px-4'
+                        >
+                            {isOpen ?
+                                <FontAwesomeIcon icon="fa-solid fa-angle-down" className="text-white bg-dark-ataju-green p-2 text-3xl rounded" />
+                                :
+                                <FontAwesomeIcon icon="fa-solid fa-bars" className="text-white bg-ataju-green p-2 text-3xl rounded" />
+                            }
+                        </button>
+                    </div>
+
+                    {isOpen && (
+                        <nav>
+
+                        </nav>
+                    )}
                 </div>
             </header>
         </>
